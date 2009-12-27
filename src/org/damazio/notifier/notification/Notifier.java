f4a8d7b9007ef96aa811cb91cd1717f4c7091ab1
rdamazio <rdamazio@localhost>
2009-12-27 22:45:45 +0000

    Adding SMS event detection.

--- src/org/damazio/notifier/notification/Notifier.java
+++ src/org/damazio/notifier/notification/Notifier.java
@@ -10,16 +10,38 @@ import android.util.Log;
 public class Notifier {
 
   private final Set<NotificationMethod> allMethods;
+  private final NotifierPreferences preferences;
 
   public Notifier(Context context, NotifierPreferences preferences) {
+    this.preferences = preferences;
     allMethods = NotificationMethods.getAllValidMethods(context, preferences);
   }
 
   public synchronized void sendNotification(Notification notification) {
-    Log.d("RemoteNotifier", "Sending notification: " + notification);
+    if (!isNotificationEnabled(notification)) {
+      return;
+    }
 
+    Log.d("RemoteNotifier", "Sending notification: " + notification);
     for (NotificationMethod method : allMethods) {
       method.sendNotification(notification);
+    }
+  }
+
+  private boolean isNotificationEnabled(Notification notification) {
+    switch (notification.getType()) {
+      case RING:
+        return preferences.isRingEventEnabled();
+      case SMS:
+        return preferences.isSmsEventEnabled();
+      case MMS:
+        return preferences.isMmsEventEnabled();
+      case BATTERY:
+        return preferences.isBatteryEventEnabled();
+      case PING:
+        return true;
+      default:
+        return false;
     }
   }
 }
--- src/org/damazio/notifier/service/NotificationService.java
+++ src/org/damazio/notifier/service/NotificationService.java
@@ -1,13 +1,19 @@
 package org.damazio.notifier.service;
 
+import java.util.List;
+
 import org.damazio.notifier.NotifierPreferences;
 import org.damazio.notifier.notification.Notification;
 import org.damazio.notifier.notification.NotificationType;
 import org.damazio.notifier.notification.Notifier;
 
+import android.app.ActivityManager;
 import android.app.Service;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
@@ -15,13 +21,17 @@ import android.util.Log;
 
 public class NotificationService extends Service {
 
+  // TODO(rdamazio): Replace this method with an AIDL binding?
+  private static NotificationService runningInstance = null;
+ 
   private NotifierPreferences preferences;
   private Notifier notifier;
+  private Handler instanceHandler;
+
   private final PhoneStateListener phoneListener = new PhoneStateListener() {
     @Override
     public void onCallStateChanged(int state, String incomingNumber) {
-      if (state == TelephonyManager.CALL_STATE_RINGING && notifier != null
-          && preferences.isRingEventEnabled()) {
+      if (state == TelephonyManager.CALL_STATE_RINGING && notifier != null) {
         Notification notification = new Notification(
             NotificationService.this, NotificationType.RING, incomingNumber);
         notifier.sendNotification(notification);
@@ -29,11 +39,25 @@ public class NotificationService extends Service {
     }
   };
 
+  public static NotificationService getRunningInstance() {
+    return runningInstance;
+  }
+
+  public void sendNotification(final Notification notification) {
+    instanceHandler.post(new Runnable() {
+      public void run() {
+        notifier.sendNotification(notification);
+      }
+    });
+  }
+
   @Override
   public void onStart(Intent intent, int startId) {
     super.onStart(intent, startId);
 
     Log.i("RemoteNotifier", "Starting notification service");
+    runningInstance = this;
+    instanceHandler = new Handler();
 
     preferences = new NotifierPreferences(this);
     notifier = new Notifier(this, preferences);
@@ -47,6 +71,7 @@ public class NotificationService extends Service {
     final TelephonyManager tm = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
     tm.listen(phoneListener, PhoneStateListener.LISTEN_NONE);
 
+    runningInstance = null;
     super.onDestroy();
   }
 
@@ -57,5 +82,24 @@ public class NotificationService extends Service {
 
   public static void start(Context context) {
     context.startService(new Intent(context, NotificationService.class));
+  }
+  
+  public static boolean isRunning(Context context) {
+    ActivityManager activityManager = (ActivityManager)context.getSystemService(ACTIVITY_SERVICE);
+    List<RunningServiceInfo> services = activityManager.getRunningServices(Integer.MAX_VALUE);
+    
+    for (RunningServiceInfo serviceInfo : services) {
+      ComponentName componentName = serviceInfo.service;
+      String serviceName = componentName.getClassName();
+      if (serviceName.equals(NotificationService.class.getName())) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public static void stop(Context context) {
+    context.stopService(new Intent(context, NotificationService.class));
   }
 }
