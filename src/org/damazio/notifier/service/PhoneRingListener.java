a5ea53dc11b22aaf45956477c02b1a3b33b9b887
rdamazio <rdamazio@localhost>
2009-12-30 18:02:05 +0000

    Refactoring how event listeners communicate with the service

--- src/org/damazio/notifier/service/PhoneRingListener.java
+++ src/org/damazio/notifier/service/PhoneRingListener.java
@@ -3,38 +3,28 @@
  */
 package org.damazio.notifier.service;
 
-import org.damazio.notifier.NotifierConstants;
 import org.damazio.notifier.notification.Notification;
 import org.damazio.notifier.notification.NotificationType;
 
-import android.content.Context;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.util.Log;
 
 /**
  * Listener which can detect and notify when the phone rings.
  *
  * @author rdamazio
  */
-public class PhoneRingListener extends PhoneStateListener {
-  private final Context context;
+class PhoneRingListener extends PhoneStateListener {
+  private final NotificationService service;
   
-  public PhoneRingListener(Context context) {
-    super();
-    this.context = context;
+  public PhoneRingListener(NotificationService context) {
+    this.service = context;
   }
 
   @Override
   public void onCallStateChanged(int state, String incomingNumber) {
-    NotificationService service = NotificationService.getRunningInstance();
-    if (service == null) {
-      Log.w(NotifierConstants.LOG_TAG, "Phone is ringing but service was not found");
-      return;
-    }
-
     if (state == TelephonyManager.CALL_STATE_RINGING) {
-      Notification notification = new Notification(context, NotificationType.RING, incomingNumber);
+      Notification notification = new Notification(service, NotificationType.RING, incomingNumber);
       service.sendNotification(notification);
     }
   }
