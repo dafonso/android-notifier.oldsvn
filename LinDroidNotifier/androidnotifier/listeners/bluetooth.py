2fa6a7de7861740f0667dfb5576538174971734d
wcauchois <wcauchois@localhost>
2010-04-09 06:41:39 +0000

    Added my BluetoothListener.

--- LinDroidNotifier/androidnotifier/listeners/bluetooth.py
+++ LinDroidNotifier/androidnotifier/listeners/bluetooth.py
@@ -6,24 +6,64 @@ This listener listens on and receives notifications with bluetooth RFCOMM
 sockets.
 """
 
-__author__ = 'rodrigo@damazio.org (Rodrigo Damazio Bovendorp)'
+__author__ = 'wcauchois@gmail.com (William Cauchois)'
 
+import bluetooth
+import gtk
 from gobject import GObject
 import gobject
+import threading
+from threading import Thread
 
-
-# TODO: Implement
-
-class BluetoothListener(GObject):
+class BluetoothListener(GObject, Thread):
+    port = 1 # The Bluetooth port for the server to listen on
+    service_id = '7674047E-6E47-4BF0-831F-209E3F9DD23F'
+    service_classes = ['7674047e-6e47-4bf0-831f-209e3f9dd23f']
+    # The timeout in seconds for the RFCOMM socket. This affects how
+    # frequently the listener thread can monitor the stopping event.
+    sock_timeout = 0.5
 
     def __init__(self):
         GObject.__init__(self)
+        Thread.__init__(self)
+
+        # Stop the thread using an event; from
+        # <http://stackoverflow.com/questions/323972/is-there-any-way-
+        # to-kill-a-thread-in-python>
+        self._stop = threading.Event()
+
+    def publish_service(self):
+        self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.sock.bind(('', self.port))
+        self.sock.listen(bluetooth.PORT_ANY)
+        bluetooth.advertise_service(self.sock, 'AndroidNotifierService',
+                                    self.service_id, self.service_classes)
+
+    def unpublish_service(self):
+        self.sock.close()
+
+    def run(self):
+        self.sock.settimeout(self.sock_timeout)
+        while not self._stop.is_set():
+            try:
+                client_sock, address = self.sock.accept()
+            except bluetooth.BluetoothError:
+                continue # The socket timed out
+            data = buffer = client_sock.recv(1024)
+            while len(buffer) == 1024:
+                buffer = client_sock.recv(1024)
+                data += buffer
+                if buffer == '':
+                    break
+            self.emit('android-notify', data)
 
     def start(self):
-        pass
+        self.publish_service()
+        Thread.start(self)
 
     def stop(self):
-        pass
+        self._stop.set()
+        self.unpublish_service()
 
 gobject.type_register(BluetoothListener)
 gobject.signal_new('android-notify', BluetoothListener,
