a6d5ef045910d4c9ee8d61a27392c8231d1c6576
rdamazio <rdamazio@localhost>
2010-04-22 02:30:51 +0000

    Adding support for v2 protocol (even though the android app doesn't use it yet - this way we give people time to update their mac apps)

--- MacDroidNotifier/Notification.h
+++ MacDroidNotifier/Notification.h
@@ -24,6 +24,7 @@ typedef enum {
   NSString *notificationId;
   NotificationType type;
   NSString *contents;
+  NSString *data;
 }
 
 // The unique ID for the device that sent the notification
@@ -35,19 +36,27 @@ typedef enum {
 // The type of notification
 @property (nonatomic,readonly) NotificationType type;
 
-// The textual contents of the notification
+// The textual (human-readable) contents of the notification
 @property (nonatomic,readonly) NSString *contents;
+
+// The non-human-readable data about the notification
+@property (nonatomic,readonly) NSString *data;
 
 // Parse a serialized notification string into a new notification object.
 + (Notification *)notificationFromString:(NSString *)serialized;
 
-// Compares this notification to another and tells if they're equal
+// Compares this notification to another and tells if they're equal.
 - (BOOL)isEqualToNotification:(Notification *)notification;
 
-// Returns the raw encoded form of the notification
+// Returns the raw encoded form of the notification.
 - (NSString *)rawNotificationString;
 
-// Returns the string equivalent of the type enum
+// Returns the string equivalent of the type enum.
 + (NSString *)stringFromNotificationType:(NotificationType)type;
+
+// Sets the given type to the equivalent of the given string.
+// Returns YES if the string was a valid type, NO otherwise.
++ (BOOL)setNotificationType:(NotificationType *)type
+                 fromString:(NSString *)typeStr;
 
 @end
--- MacDroidNotifier/Notification.m
+++ MacDroidNotifier/Notification.m
@@ -14,16 +14,19 @@
 @synthesize notificationId;
 @synthesize type;
 @synthesize contents;
+@synthesize data;
 
 - (id)initWithDeviceId:(NSString *)deviceIdParam
     withNotificationId:(NSString *)notificationIdParam
               withType:(NotificationType)typeParam
-          withContents:(NSString *)contentsParam {
+          withContents:(NSString *)contentsParam
+              withData:(NSString *)dataParam {
   if (self = [super init]) {
     deviceId = [deviceIdParam copy];
     notificationId = [notificationIdParam copy];
     type = typeParam;
     contents = [contentsParam copy];
+    data = [dataParam copy];
   }
   return self;
 }
@@ -32,67 +35,111 @@
   [deviceId release];
   [notificationId release];
   [contents release];
+  [data release];
   [super dealloc];
 }
 
-+ (Notification *)notificationFromString:(NSString *)serialized {
-  NSArray *parts = [serialized pathComponents];
-
-  NSString *deviceId = [parts objectAtIndex:0];
-  NSString *notificationId = [parts objectAtIndex:1];
-  NSString *typeStr = [parts objectAtIndex:2];
-  NSString *contents;
-
++ (NSString *)descriptionFromParts:(NSArray *)parts
+                        startingAt:(NSUInteger)firstPart
+                    withTotalParts:(NSUInteger)totalParts {
   NSUInteger numParts = [parts count];
-  if (numParts < 4) {
-    NSLog(@"Malformed notification: '%@'", serialized);
-    return nil;
-  } else if (numParts > 4) {
-    // Oops, we broke down the description, put it back together as the last item
-    NSArray *contentsParts = [parts subarrayWithRange:NSMakeRange(3, numParts - 3)];
+  NSString *contents;
+  if (numParts > totalParts) {
+    // Oops, we broke down the description, put it back together
+    NSRange contentRange = NSMakeRange(firstPart, numParts - totalParts + 1);
+    NSArray *contentsParts = [parts subarrayWithRange:contentRange];
     contents = [NSString pathWithComponents:contentsParts];
   } else {
-    contents = [parts objectAtIndex:3];
+    contents = [parts objectAtIndex:firstPart];
     if ([contents isEqualToString:@"/"]) contents = @"";
   }
+  return contents;
+}
+
++ (Notification *)notificationFromV2Parts:(NSArray *)parts {
+  NSUInteger numParts = [parts count];
+  if (numParts < 6) {
+    return nil;
+  }
+
+  // Part 0 is "v2"
+  NSString *deviceId = [parts objectAtIndex:1];
+  NSString *notificationId = [parts objectAtIndex:2];
+  NSString *typeStr = [parts objectAtIndex:3];
+  NSString *data = [parts objectAtIndex:4];
+  NSString *contents = [self descriptionFromParts:parts
+                                       startingAt:5
+                                   withTotalParts:6];
 
   NotificationType type;
-  if ([typeStr isEqualToString:@"RING"]) {
-    type = RING;
-  } else if ([typeStr isEqualToString:@"BATTERY"]) {
-    type = BATTERY;
-  } else if ([typeStr isEqualToString:@"SMS"]) {
-    type = SMS;
-  } else if ([typeStr isEqualToString:@"MMS"]) {
-    type = MMS;
-  } else if ([typeStr isEqualToString:@"VOICEMAIL"]) {
-    type = VOICEMAIL;
-  } else if ([typeStr isEqualToString:@"PING"]) {
-    type = PING;
-  } else {
-    NSLog(@"Malformed notification: '%@'", serialized);
+  if (![self setNotificationType:&type fromString:typeStr]) {
     return nil;
   }
 
   return [[[Notification alloc] initWithDeviceId:deviceId
                               withNotificationId:notificationId
                                         withType:type
-                                    withContents:contents] autorelease];
+                                    withContents:contents
+                                        withData:data] autorelease];
+}
+
++ (Notification *)notificationFromV1Parts:(NSArray *)parts {
+  NSUInteger numParts = [parts count];
+  if (numParts < 4) {
+    return nil;
+  }
+
+  NSString *deviceId = [parts objectAtIndex:0];
+  NSString *notificationId = [parts objectAtIndex:1];
+  NSString *typeStr = [parts objectAtIndex:2];
+  NSString *contents = [self descriptionFromParts:parts
+                                       startingAt:3
+                                   withTotalParts:4];
+
+  NotificationType type;
+  if (![self setNotificationType:&type fromString:typeStr]) {
+    return nil;
+  }
+
+  return [[[Notification alloc] initWithDeviceId:deviceId
+                              withNotificationId:notificationId
+                                        withType:type
+                                    withContents:contents
+                                        withData:@""] autorelease];
+}
+
++ (Notification *)notificationFromString:(NSString *)serialized {
+  NSArray *parts = [serialized pathComponents];
+
+  Notification *result;
+  if ([parts count] > 0 &&
+      [[parts objectAtIndex:0] isEqualToString:@"v2"]) {
+    result = [self notificationFromV2Parts:parts];
+  } else {
+    result = [self notificationFromV1Parts:parts];
+  }
+
+  if (!result) {
+    NSLog(@"Malformed notification: '%@'", serialized);
+  }
+
+  return result;
 }
 
 - (BOOL)isEqualToNotification:(Notification *)notification {
   return [[notification deviceId] isEqualToString:[self deviceId]] &&
          [[notification notificationId] isEqualToString:[self notificationId]] &&
          ([notification type] == type) &&
-         [[notification contents] isEqualToString:[self contents]];
+         [[notification contents] isEqualToString:[self contents]] &&
+         [[notification data] isEqualToString:[self data]];
 }
 
 - (NSString *)description {
   NSString *typeStr = [Notification stringFromNotificationType:type];
   if (!typeStr) typeStr = @"UNKNOWN";
 
-  return [NSString stringWithFormat:@"Id=%@; DeviceId=%@; Type=%@; Contents=%@",
-          notificationId, deviceId, typeStr, contents];
+  return [NSString stringWithFormat:@"Id=%@; DeviceId=%@; Type=%@; Data=%@; Contents=%@",
+          notificationId, deviceId, typeStr, data, contents];
 }
 
 - (NSString *)rawNotificationString {
@@ -101,6 +148,26 @@
   
   return [NSString stringWithFormat:@"%@/%@/%@/%@",
           deviceId, notificationId, typeStr, contents];  
+}
+
++ (BOOL)setNotificationType:(NotificationType *)type
+                 fromString:(NSString *)typeStr {
+  if ([typeStr isEqualToString:@"RING"]) {
+    *type = RING;
+  } else if ([typeStr isEqualToString:@"BATTERY"]) {
+    *type = BATTERY;
+  } else if ([typeStr isEqualToString:@"SMS"]) {
+    *type = SMS;
+  } else if ([typeStr isEqualToString:@"MMS"]) {
+    *type = MMS;
+  } else if ([typeStr isEqualToString:@"VOICEMAIL"]) {
+    *type = VOICEMAIL;
+  } else if ([typeStr isEqualToString:@"PING"]) {
+    *type = PING;
+  } else {
+    return NO;
+  }
+  return YES;
 }
 
 + (NSString *)stringFromNotificationType:(NotificationType)type {
