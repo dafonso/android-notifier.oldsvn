d580fe9da33edb5dbcb7ffd391887022a5ef2f55
rdamazio <rdamazio@localhost>
2010-05-03 06:16:40 +0000

    Stopping Android from displaying a hang message for notifier at startup

--- AndroidNotifier/src/org/damazio/notifier/service/BootServiceStarter.java
+++ AndroidNotifier/src/org/damazio/notifier/service/BootServiceStarter.java
@@ -1,11 +1,13 @@
 package org.damazio.notifier.service;
 
+import org.damazio.notifier.NotifierConstants;
 import org.damazio.notifier.NotifierPreferences;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.SystemClock;
+import android.os.Handler;
+import android.util.Log;
 
 /**
  * Receiver for boot events, which starts the service if the user chose to have
@@ -17,9 +19,10 @@ public class BootServiceStarter extends BroadcastReceiver {
   private static final double MAX_STARTUP_DELAY_MS = 30000.0;
 
   @Override
-  public void onReceive(Context context, Intent intent) {
+  public void onReceive(final Context context, Intent intent) {
     NotifierPreferences preferences = new NotifierPreferences(context);
     if (!preferences.isStartAtBootEnabled()) {
+      Log.d(NotifierConstants.LOG_TAG, "Not starting at boot.");
       return;
     }
 
@@ -27,8 +30,12 @@ public class BootServiceStarter extends BroadcastReceiver {
 
     // Wait some random time before starting the service - we don't really need
     // to make the system boot slower by having it start right away.
-    SystemClock.sleep((long) (Math.random() * MAX_STARTUP_DELAY_MS));
-
-    NotificationService.start(context);
+    long waitMs = (long) (Math.random() * MAX_STARTUP_DELAY_MS);
+    Log.d(NotifierConstants.LOG_TAG, "Starting at boot after " + waitMs + "ms");
+    new Handler().postDelayed(new Runnable() {
+      public void run() {
+        NotificationService.start(context);
+      }
+    }, waitMs);
   }
 }
