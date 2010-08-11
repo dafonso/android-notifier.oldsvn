e1b22f2c5a2bd3b05c19db306b7c7a6287ae23f1
wcauchois <wcauchois@localhost>
2010-08-11 02:14:22 +0000

    implemented support for V2 of the notifications protocol

--- LinDroidNotifier/androidnotifier/notification.py
+++ LinDroidNotifier/androidnotifier/notification.py
@@ -11,10 +11,29 @@ class Notification:
     def __init__(self, serialized):
         self.serialized = serialized
         parts = serialized.split('/')
+        if parts[0] == 'v2':
+            self._parse_v2_notification(parts)
+        else:
+            self._parse_v1_notification(parts)
+
+    def _parse_v2_notification(self, parts):
+        # parts[0] is just "v2"
+        self.device_id = parts[1]
+        self.notification_id = parts[2]
+        self.event_type = parts[3]
+        self.data = parts[4]
+        self.contents = '/'.join(parts[5:])
+
+    def _parse_v1_notification(self, parts):
         self.device_id = parts[0]
         self.notification_id = parts[1]
         self.event_type = parts[2]
         self.contents = '/'.join(parts[3:])
+        self.data = None
+
+    @property
+    def data(self):
+        return self.data
 
     @property
     def device_id(self):
