9cd2770921d32ecb05ca1a8c4c2ddf656405a249
rdamazio <rdamazio@localhost>
2010-09-27 05:06:40 +0000

    Adding first version of locale plugin for sending notifications

---
+++ src/org/damazio/notifier/locale/notify/FireReceiver.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2010 Rodrigo Damazio <rodrigo@damazio.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.damazio.notifier.locale.notify;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Receiver which executes settings changes when requested by Locale.
+ *
+ * @author rdamazio
+ */
+public class FireReceiver extends BroadcastReceiver {
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    if (!com.twofortyfouram.Intent.ACTION_FIRE_SETTING.equals(intent.getAction())) {
+      Log.e(Constants.LOG_TAG, "Got wrong intent: " + intent.getAction());
+      return;
+    }
+
+    Intent broadcastIntent = new Intent(Constants.BROADCAST_ACTION);
+    Bundle extras = intent.getExtras();
+    if (extras == null) {
+      Log.e(Constants.LOG_TAG, "No extras when firing!");
+      return;
+    }
+
+    broadcastIntent.putExtras(extras);
+    context.sendBroadcast(broadcastIntent);
+    Log.d(Constants.LOG_TAG, "Sent remote notification");
+  }
+}
