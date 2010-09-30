204aca5b8ee93c4b7a3c7b0eb135f1bbb4d1e292
lehphyro <lehphyro@localhost>
2010-09-30 14:49:03 +0000

    Protocol buffers notification parser

---
+++ MultiDroidNotifier/src/main/java/com/notifier/desktop/exception/ParseException.java
@@ -0,0 +1,10 @@
+package com.notifier.desktop.exception;
+
+public class ParseException extends Exception {
+
+	private static final long serialVersionUID = -948199427771782580L;
+
+	public ParseException(Throwable cause) {
+		super(cause);
+	}
+}
