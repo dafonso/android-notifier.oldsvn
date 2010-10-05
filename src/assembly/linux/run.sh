5f99d0ae83dba2d5a1e1d0f79e73b78f26aa10e6
lehphyro <lehphyro@localhost>
2010-10-05 19:10:40 +0000

    Linux JVM does not like when prefs.xml files are deleted

--- src/assembly/linux/run.sh
+++ src/assembly/linux/run.sh
@@ -7,11 +7,4 @@ else
   configDir=$XDG_CONFIG_HOME
 fi
 
-# Migrate existing configuration to conform with XDG standard
-mkdir -p $configDir/android-notifier-desktop/.java/.userPrefs/com/google/code/notifier/desktop
-if [ -d  ~/.java/.userPrefs/com/google/code/notifier ]; then
-  mv -f ~/.java/.userPrefs/com/google/code/notifier/desktop/prefs.xml $configDir/android-notifier-desktop/.java/.userPrefs/com/google/code/notifier/desktop
-  rm -fr ~/.java/.userPrefs/com/google/code/notifier
-fi
-
 java -DconfigDir=$configDir -Djava.util.prefs.userRoot=$configDir/android-notifier-desktop -Djava.net.preferIPv4Stack=true -client -Xms8m -Xmx32m -jar ${assembly.jar.path} $1
